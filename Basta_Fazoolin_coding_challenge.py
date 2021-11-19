3288d6ad336e47e684936ab108ae18def69c6d98
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-11-19 10:24:11 -0500

    Init Commit

---
+++ Basta_Fazoolin_coding_challenge.py
@@ -0,0 +1,7 @@
+class Menu:
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+    
